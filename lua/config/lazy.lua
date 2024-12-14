7ca6405241501e3d2bb5f63cfbb575a237ae9ab3
Miguel Duarte <universesaurora@gmail.com>
2024-12-14 20:54:13 +0800

    neovim add indent-blankline to setup

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -64,6 +64,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.ui.treesitter-context" },
     { import = "lazyvim.plugins.extras.util.project" },
+    { import = "lazyvim.plugins.extras.ui.indent-blankline" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
