8dfde55dc482a1a7bdadf8cf0b7791879019804e
UniversesAurora <universesaurora@gmail.com>
2026-01-06 10:05:55 +0800

    nvim-lspconfig use different clangd executable

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,23 @@
 return {
---   {
---     "neovim/nvim-lspconfig",
---     opts = {},
---   },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --     -- Use different clangd executable
+  --       clangd = {
+  --         cmd = {
+  --           "/opt/homebrew/opt/llvm/bin/clangd",
+  --           "--background-index",
+  --           "--clang-tidy",
+  --           "--header-insertion=never",
+  --           "--completion-style=detailed",
+  --           "--function-arg-placeholders",
+  --           "--fallback-style=llvm",
+  --           "--clang-tidy-checks=*",
+  --         },
+  --         mason = false,
+  --       },
+  --     },
+  --   },
+  -- },
 }
