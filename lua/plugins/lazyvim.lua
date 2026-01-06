9978c2cbedf5c0be292d7a2fbe82d3c955f2fa42
UniversesAurora <universesaurora@gmail.com>
2026-01-06 10:16:39 +0800

    LazyVim default disable kind_filter

--- lua/plugins/lazyvim.lua
+++ lua/plugins/lazyvim.lua
@@ -5,6 +5,11 @@ return {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "sweetie",
+
+      -- https://github.com/LazyVim/LazyVim/issues/2216#issuecomment-2091460516
+      kind_filter = {
+        default = false,
+      },
     },
   },
 }
